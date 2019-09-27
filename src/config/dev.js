8cd725f905bb42210800a4d22f117c1e767007fb
Sonia Jain <baps@bapss-MacBook-Pro-2.local>
2019-09-27 12:59:46 +0530

    firebase redux app

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBGxYzXtasBnsoQH0yf_KEE4UhlWi8421Y",
+    authDomain: "firedux-todo-d20eb.firebaseapp.com",
+    databaseURL: "https://firedux-todo-d20eb.firebaseio.com",
+    projectId: "firedux-todo-d20eb",
+    storageBucket: "",
+    messagingSenderId: "937376211440",
+  }
